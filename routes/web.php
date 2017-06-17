62ae5f73f209b0e4398365829d95f500ff892f65
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-06-17 00:34:57 -0700

    using dot notation

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,20 @@ use Symfony\Component\Translation\Loader\ArrayLoader;
 $app->get('/home', HomeController::class . ':index');
 
 $app->get('/', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
-
+    
+    $translations = [
+        'key'       => 'tranlation',
+        'greetings' => [
+            'welcome' => 'Welcome',
+        ],
+        'messages'  => [
+            'new_message' => 'You got a new message',
+        ],
+    
+    ];
     $trnaslation = new Symfony\Component\Translation\Translator('en');
     $trnaslation->addLoader('array', new ArrayLoader());
-    $trnaslation->addResource('array', [
-        'welcome' => 'Welcome'
-    ], 'en');
+    $trnaslation->addResource('array', $translations, 'en');
     
-    dump($trnaslation->trans('welcome'));
+    dump($trnaslation->trans('messages.new_message'));
 });
