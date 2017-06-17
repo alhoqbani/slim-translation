cdce632a2a7ecb46bacb03dd6c3a91d4794f43c3
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-06-17 02:16:18 -0700

    Move tranlator to slim container

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -1,5 +1,8 @@
 <?php
 
+use Symfony\Component\Translation\Loader\ArrayLoader;
+use Symfony\Component\Translation\Translator;
+
 $container = $app->getContainer();
 
 $container['view'] = function ($c) {
@@ -24,4 +27,30 @@ $container['db'] = function ($c) {
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
     
     return $pdo;
+};
+
+$container['translator'] = function ($c) {
+    $ar = ROOT . 'resources/lang/ar/greetings.json';
+    
+    $ar = [
+        'greetings' => require ROOT . 'resources/lang/ar/greetings.php',
+        'messages'  => require ROOT . 'resources/lang/ar/messages.php',
+    ];
+    
+    $en = [
+        'greetings' => require ROOT . 'resources/lang/en/greetings.php',
+        'messages'  => require ROOT . 'resources/lang/en/messages.php',
+    ];
+    
+    // Choose on locale:
+    $translator = new Translator('ar');
+    
+    $translator->setFallbackLocales(['ar', 'en']);
+    
+    $translator->addLoader('array', new ArrayLoader());
+    
+    $translator->addResource('array', $en, 'en');
+    $translator->addResource('array', $ar, 'ar');
+    
+    return $translator;
 };
--- routes/web.php
+++ routes/web.php
@@ -8,41 +8,20 @@ use Symfony\Component\Translation\Loader\ArrayLoader;
 $app->get('/home', HomeController::class . ':index');
 
 $app->get('/', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
-    $ar = ROOT . 'resources/lang/ar/greetings.json';
     
-    $ar = [
-        'greetings' => require ROOT . 'resources/lang/ar/greetings.php',
-        'messages'  => require ROOT . 'resources/lang/ar/messages.php',
-    ];
+    dump($this->translator->trans('greetings.good_buy'));
     
-    $en = [
-        'greetings' => require ROOT . 'resources/lang/en/greetings.php',
-        'messages'  => require ROOT . 'resources/lang/en/messages.php',
-    ];
-    
-    // Choose on locale:
-    $trnaslation = new Symfony\Component\Translation\Translator('ar');
-    
-    $trnaslation->setFallbackLocales(['ar', 'en']);
-    
-    $trnaslation->addLoader('array', new ArrayLoader());
-    
-    $trnaslation->addResource('array', $en, 'en');
-    $trnaslation->addResource('array', $ar, 'ar');
-    
-    dump($trnaslation->trans('greetings.good_buy'));
-    
-    echo $trnaslation->transChoice('messages.advanced_count', 0) . '<br>';
-    echo $trnaslation->transChoice('messages.advanced_count', 1) . '<br>';
-    echo $trnaslation->transChoice('messages.advanced_count', 2) . '<br>';
-    echo $trnaslation->transChoice('messages.advanced_count', 3) . '<br>';
-    echo $trnaslation->transChoice('messages.advanced_count', 4) . '<br>';
-    echo $trnaslation->transChoice('messages.advanced_count', 9) . '<br>';
-    echo $trnaslation->transChoice('messages.advanced_count', 10) . '<br>';
-    echo $trnaslation->transChoice('messages.advanced_count', 11) . '<br>';
-    echo $trnaslation->transChoice('messages.advanced_count', 30) . '<br>';
-    echo $trnaslation->transChoice('messages.advanced_count', 100) . '<br>';
-    echo $trnaslation->transChoice('messages.advanced_count', 302) . '<br>';
+    echo $this->translator->transChoice('messages.advanced_count', 0) . '<br>';
+    echo $this->translator->transChoice('messages.advanced_count', 1) . '<br>';
+    echo $this->translator->transChoice('messages.advanced_count', 2) . '<br>';
+    echo $this->translator->transChoice('messages.advanced_count', 3) . '<br>';
+    echo $this->translator->transChoice('messages.advanced_count', 4) . '<br>';
+    echo $this->translator->transChoice('messages.advanced_count', 9) . '<br>';
+    echo $this->translator->transChoice('messages.advanced_count', 10) . '<br>';
+    echo $this->translator->transChoice('messages.advanced_count', 11) . '<br>';
+    echo $this->translator->transChoice('messages.advanced_count', 30) . '<br>';
+    echo $this->translator->transChoice('messages.advanced_count', 100) . '<br>';
+    echo $this->translator->transChoice('messages.advanced_count', 302) . '<br>';
     echo '<br><br>';
-    echo $trnaslation->trans('messages.new_message');
+    echo $this->translator->trans('messages.new_message');
 });
