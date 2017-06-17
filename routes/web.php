f6968fb590d0765ba7e13447d8a6fe59bf4fa654
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-06-17 00:27:50 -0700

    Add symfony/ranslation package

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,17 @@
 use App\Http\Controllers\HomeController;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Component\Translation\Loader\ArrayLoader;
 
 $app->get('/home', HomeController::class . ':index');
 
 $app->get('/', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
-    return $response->write(
-        'Welcome To Your Slim App
-        <br><a href="/home">Home</a>
-        <a href="/api">Api</a>'
-    );
+
+    $trnaslation = new Symfony\Component\Translation\Translator('en');
+    $trnaslation->addLoader('array', new ArrayLoader());
+    $trnaslation->addResource('array', [
+        'welcome' => 'Welcome'
+    ], 'en');
+    
+    dump($trnaslation->trans('welcome'));
 });
