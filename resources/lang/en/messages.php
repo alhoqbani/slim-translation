99ab331a106580d9b1ac67d2dbfdec70d10e7bb2
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-06-17 01:47:42 -0700

    Add Advanced Choices sytax

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -2,5 +2,6 @@
 
 return [
     'count'  => 'You have one new message|You have %count% new messages',
+    'advanced_count' => '{0} You have no messages|{1} Just One Message|]1,9[ a few messages: %count%|[9,Inf[ A LOT. You have %count% messages',
     'new_message' => 'This is a new message alert',
 ];
--- routes/web.php
+++ routes/web.php
@@ -32,17 +32,17 @@ $app->get('/', function (ServerRequestInterface $request, ResponseInterface $res
     
     dump($trnaslation->trans('greetings.good_buy'));
     
-    echo $trnaslation->transChoice('messages.count', 0) . '<br>';
-    echo $trnaslation->transChoice('messages.count', 1) . '<br>';
-    echo $trnaslation->transChoice('messages.count', 2) . '<br>';
-    echo $trnaslation->transChoice('messages.count', 3) . '<br>';
-    echo $trnaslation->transChoice('messages.count', 4) . '<br>';
-    echo $trnaslation->transChoice('messages.count', 9) . '<br>';
-    echo $trnaslation->transChoice('messages.count', 10) . '<br>';
-    echo $trnaslation->transChoice('messages.count', 11) . '<br>';
-    echo $trnaslation->transChoice('messages.count', 30) . '<br>';
-    echo $trnaslation->transChoice('messages.count', 100) . '<br>';
-    echo $trnaslation->transChoice('messages.count', 302) . '<br>';
+    echo $trnaslation->transChoice('messages.advanced_count', 0) . '<br>';
+    echo $trnaslation->transChoice('messages.advanced_count', 1) . '<br>';
+    echo $trnaslation->transChoice('messages.advanced_count', 2) . '<br>';
+    echo $trnaslation->transChoice('messages.advanced_count', 3) . '<br>';
+    echo $trnaslation->transChoice('messages.advanced_count', 4) . '<br>';
+    echo $trnaslation->transChoice('messages.advanced_count', 9) . '<br>';
+    echo $trnaslation->transChoice('messages.advanced_count', 10) . '<br>';
+    echo $trnaslation->transChoice('messages.advanced_count', 11) . '<br>';
+    echo $trnaslation->transChoice('messages.advanced_count', 30) . '<br>';
+    echo $trnaslation->transChoice('messages.advanced_count', 100) . '<br>';
+    echo $trnaslation->transChoice('messages.advanced_count', 302) . '<br>';
     echo '<br><br>';
     echo $trnaslation->trans('messages.new_message');
 });
